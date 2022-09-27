82a2007b8a4df50bc6a6411189f4e26a992bd08a
Edgar R <edgar.pfaeskillsup@gmail.com>
2022-09-27 10:02:03 +0200

    Creación de nuevas carpetas en el repositorio.

---
+++ modules/include/news_reg.php
@@ -0,0 +1,19 @@
+<?php
+    require "../require/config.php";
+
+    // define variables and set to empty values
+    $Nombre = $correo = $tlf = $calle = $ciudad = $estado = $cp = $news = $formato = $topics = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    /* $Nombre = test_input($_POST["name"]);
+    $correo = test_input($_POST["email"]);
+    $tlf = test_input($_POST["website"]);
+    $calle = test_input($_POST["comment"]);
+    $ciudad = test_input($_POST["gender"]);
+    $estado = test_input($_POST["gender"]);
+    $cp = test_input($_POST["gender"]);
+    $news = test_input($_POST["gender"]);
+    $formato = test_input($_POST["gender"]);
+    $topics = test_input($_POST["gender"]); */
+    }
+?>
