ad47b2f10a4c3120827128781bfe30125ad73f54
Edgar R <edgar.pfaeskillsup@gmail.com>
2022-09-30 09:40:45 +0200

    Se añadió la función isset() con el if en cada variable para poder asignar valor NULL si está vacía.

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -1,7 +1,7 @@
 <?php
     require "../require/config.php";
 
-    // define variables  y dar values vacío ("")
+    // Definir variables  y dar values vacío ("")
     $Nombre = $correo = $tlf = $calle = $ciudad = $estado = $cp = $news = $formato = $topics = "";
 
     // Declarar la función para limpiar
@@ -32,8 +32,8 @@
         }
 
         // Validación nº teléfono
-        function validar_Tlf($Nombre){
-            if (!preg_match("/^[0-9]{10}+$/",$tlf)) { 
+        function validar_Tlf($tlf){
+            if (!preg_match("/^[0-9]{9}+$/",$tlf)) { 
                 return false;
             } else{
                 return true;
@@ -42,33 +42,84 @@
 
     // Si (llega datos) Entonces
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        //print_r ($_POST);  // Para ver que están llegando los datos
+        print_r ($_POST);  // Variables requeridas para enviar a BBDD: nombre, correo y tlf.
         if (!empty($_POST["Nombre"]) || !empty($_POST["correo"]) || !empty($_POST["tlf"])){
             echo "<br><strong>En los campos requeridos hay datos</strong><br>";
 
-        // Asignar variables y asignar la función limpiar_dato
+        // Asignar variables y asignar la función limpiar_dato; A las variables que pueden ser NULL añadir if para guardar como NULL
             $Nombre = limpiar_dato($_POST["Nombre"]);
             $correo = limpiar_dato($_POST["correo"]);
             $tlf = limpiar_dato($_POST["tlf"]);
-            $calle = limpiar_dato($_POST["calle"]);
-            $ciudad = limpiar_dato($_POST["ciudad"]);
-            $estado = limpiar_dato($_POST["estado"]);
-            $cp = limpiar_dato($_POST["cp"]);
-            $news = limpiar_dato($_POST["news"]);
-            $formato = limpiar_dato($_POST["formato"]);
-            $topics = limpiar_dato($_POST["topics"]);
+            
+            if (isset($_POST["calle"])){
+                $calle = limpiar_dato($_POST["calle"]);
+            } else{
+                $calle = NULL;
+            }
 
-        
+            if (isset($_POST["ciudad"])){
+                $ciudad = limpiar_dato($_POST["ciudad"]);
+            } else{
+                $ciudad = NULL;
+            }
+
+            if (isset($_POST["estado"])){
+                $estado = limpiar_dato($_POST["estado"]);
+            } else{
+                $estado = NULL;
+            }
+
+            if (isset($_POST["cp"])){
+                $cp = limpiar_dato($_POST["cp"]);
+            } else{
+                $cp = NULL;
+            } 
+
+            if (isset($_POST["news"])){
+                $news = limpiar_dato($_POST["news"]);
+            } else{
+                $news = NULL;
+            }
+
+            if (isset($_POST["formato"])){
+                $formato = limpiar_dato($_POST["formato"]);
+            } else{
+                $formato = NULL;
+            }
+
+            if (isset($_POST["topics"])){
+                $topics = limpiar_dato($_POST["topics"]);
+            } else{
+                $topics = NULL;
+            }
+// ============================================================= BORRAME
+
         echo "<br><strong>Name: </strong>".$Nombre ."<br>";
-        echo "<br><strong>Email: </strong>".$correo ."<br>";
-        echo "<br><strong>Teléfono: </strong>".$tlf ."<br>";
 
         if (validar_Nombre($Nombre)){
-            echo "La validación de Name está hecha";
+            echo "La validación de Name está hecha<br>";
         } else{
-            echo "Name no válido";
+            echo "Name no válido<br>";
         }
-        // TO DO seguir con los if para comprobar que se validan
+
+        echo "<br><strong>Email: </strong>".$correo ."<br>";
+
+        if (validar_Email($correo)){
+            echo "La validación de Email está hecha<br>";
+        } else{
+            echo "Email no válido<br>";
+        }
+
+        echo "<br><strong>Teléfono: </strong>".$tlf ."<br>";
+
+        if (validar_Tlf($tlf)){
+            echo "La validación de Teléfono está hecha<br>";
+        } else{
+            echo "Teléfono no válido<br>";
+        }
+
+
+// ============================================================= BORRAME
         }
     }
 ?>
